cc1f4fec9fcf2d6d1d6373e11c46ca57cdf64303
CowKeyMan <cowkeyman@gmail.com>
2026-08-11 20:51:05 +0300

    Replace auto_session with persisted

---
+++ lua/plugins/persisted.lua
@@ -0,0 +1,29 @@
+-- save and restore vim
+--
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+return {
+  "olimorris/persisted.nvim",
+  lazy = false,
+  opts = {
+    -- 1. Main configuration behavior
+    autostart = true,
+    save_dir = vim.fn.stdpath("data") .. "/my_custom_sessions/" .. vim.loop.os_uname().sysname,
+
+    -- 2. DYNAMIC AUTOLOAD:
+    -- Only auto-load the session if zero command line arguments are given.
+    -- If you run 'nvim file.txt', argc() is 1, turning autoload off entirely.
+    autoload = vim.fn.argc() == 0, 
+
+    -- Terminal safety hooks
+    on_autoload_callback = function()
+      vim.cmd("doautocmd TermOpen")
+    end,
+  },
+  config = function(_, opts)
+    if vim.fn.isdirectory(opts.save_dir) == 0 then
+      vim.fn.mkdir(opts.save_dir, "p")
+    end
+    require("persisted").setup(opts)
+  end,
+}
